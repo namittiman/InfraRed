8bf4b4ca7321cba57310b448b18469db315290cb
Namit Sharma <nsharm11@ncsu.edu>
2016-10-09 20:05:45 -0400

    Setting up basic node.js application for launcing instances on AWS

---
+++ aws_nodejs/create_instance.js
@@ -0,0 +1,29 @@
+// Load the SDK and UUID
+var AWS = require('aws-sdk');
+var uuid = require('node-uuid');
+AWS.config.region = 'us-east-1';
+
+var ec2 = new AWS.EC2();
+
+var params = {
+  ImageId: 'ami-1624987f', // Amazon Linux AMI x86_64 EBS
+  InstanceType: 't1.micro',
+  MinCount: 1, MaxCount: 1
+};
+
+// Create the instance
+ec2.runInstances(params, function(err, data) {
+  if (err) { console.log("Could not create instance", err); return; }
+
+  console.log(data);
+  var instanceId = data.Instances[0].InstanceId;
+  console.log("Created instance", instanceId);
+
+  // Add tags to the instance
+  params = {Resources: [instanceId], Tags: [
+    {Key: 'Name', Value: 'instanceName'}
+  ]};
+  ec2.createTags(params, function(err) {
+    console.log("Tagging instance", err ? "failure" : "success");
+  });
+});
